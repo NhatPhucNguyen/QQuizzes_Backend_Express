5d1af8251a193a67d7c07c6f1ea5d7493a6b46bd
pnguy118 <phucnhat2111@gmail.com>
2023-07-12 23:23:25 +0700

    Initializing, set up dev env, connect to local database using mongoose

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import { Express } from "express";
+import express from "express";
+import "dotenv/config";
+import morgan from "morgan";
+import mongoose from "mongoose";
+const app: Express = express();
+const PORT = process.env.PORT || 5000;
+const mongoString =
+    process.env.DATABASE_URI || "mongodb://127.0.0.1:27017/QQuizzes";
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+database.on("error", (err) => {
+    console.log(err);
+});
+database.once("connected", () => {
+    console.log("Connected to database...");
+});
+app.use(morgan("tiny"));
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+app.listen(PORT, () => {
+    console.log("Sever is listening on port:", PORT);
+});
