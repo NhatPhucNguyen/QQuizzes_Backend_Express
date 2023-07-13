104602e54b9c670ab4d859fd110a6ffb10cb553a
pnguy118 <phucnhat2111@gmail.com>
2023-07-13 15:56:30 +0700

    comment important parts

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,14 +3,17 @@ import { IUser } from "../interfaces/db_interfaces";
 import User from "../models/user";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+
 export const handleRegister = async (req: Request, res: Response) => {
     const newUser = req.body as IUser;
+    //checking required fields
     if (!newUser.username || !newUser.password || !newUser.email) {
         return res.status(400).json({
             message: "Missing required fields.",
         });
     }
     try {
+        //make sure user does not exist
         const duplicatedUser = await User.findOne({
             username: newUser.username,
             email: newUser.email,
@@ -49,12 +52,14 @@ export const handleLogin = async (req: Request, res: Response) => {
             message: "User does not exist.",
         });
     }
+    //compare input password vs hashed password in database
     const isMatch = await bcrypt.compare(password, foundUser.password);
     if (!isMatch) {
         return res.status(404).json({
             message: "Password is incorrect.",
         });
     } else {
+        //generate access token
         const accessToken = jwt.sign(
             {
                 userId: foundUser._id,
@@ -62,6 +67,7 @@ export const handleLogin = async (req: Request, res: Response) => {
             process.env.ACCESS_TOKEN_SECRET || "",
             { expiresIn: process.env.DEV_EXPIRE || "15m" }
         );
+        //generate refresh token to save in database
         const refreshToken = jwt.sign(
             {
                 userId: foundUser._id,
@@ -73,12 +79,13 @@ export const handleLogin = async (req: Request, res: Response) => {
             await User.findByIdAndUpdate(foundUser._id, {
                 refreshToken: refreshToken,
             });
+            //setup cookies
             res.cookie("jwt", refreshToken, {
-                httpOnly: true,
+                httpOnly: true, //http only to prevent access by using JS
                 maxAge: 24 * 60 * 60 * 1000,
                 sameSite: "none",
                 secure: true,
-            });
+            }); 
             return res.status(200).json({
                 accessToken,
             });
